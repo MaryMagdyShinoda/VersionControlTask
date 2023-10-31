3409ee97e6d527260ab1785107a453cb147efa6d
mary <marymagdy530@gmail.com>
2023-10-31 18:37:42 +0200

    Added getAverage function

--- main.cpp
+++ main.cpp
@@ -7,20 +7,32 @@ int getSum(vector<int>numbers)
 {
 	int numbersSummation=0;
 	for (int i = 0; i < numbers.size(); i++){
-		numbersSummation += numbers[i];	
+		numbersSummation += numbers[i];
 	}
 	return numbersSummation;
 }
+
+double getAverage(vector<int>numbers)
+{
+	int numbersSummation = getSum(numbers);
+	double numbersAverage = (double)numbersSummation / numbers.size();
+	return numbersAverage;
+}
+
 int main()
 {
 	vector<int> numbers;
-	     
+
         for (int i = 1; i <= 10; i++){
 		numbers.push_back(i);
 	}
 	int numbersSummation;
 	numbersSummation = getSum(numbers);
 	cout<<numbersSummation<<endl;
+
+	double numbersAverage;
+	numbersAverage = getAverage(numbers);
+	cout<<numbersAverage<<endl;
 	return 0;
 }
 
